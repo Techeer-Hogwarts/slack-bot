9bf93bfd694818861b0ae089dc0bd8f70eda7e4e
Ryan <leehahnwoong@gmail.com>
2024-07-27 22:24:28 -0700

    fix

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/thomas-and-friends/slack-bot/cmd"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	http.HandleFunc("/slack/commands", cmd.HandleSlashCommand)
 	http.HandleFunc("/trigger_event", cmd.TriggerEvent)
 	http.HandleFunc("/", cmd.SendHelloWorld)
 
-	log.Println("Server started on :3000")
+	log.Printf("Server started on port: %v", port)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
