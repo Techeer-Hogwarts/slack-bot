f83e04701fc1457e54e85c38b6021342201781d0
Ryan <leehahnwoong@gmail.com>
2024-07-28 18:28:16 -0700

    testing

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -8,28 +8,37 @@ import (
 	"github.com/slack-go/slack"
 )
 
+type FormMessage struct {
+	FormID        string   `json:"form_id"`
+	TeamIntro     string   `json:"intro"`
+	TeamName      string   `json:"name"`
+	TeamRoles     []string `json:"roles"`
+	TechStacks    []string `json:"tech"`
+	Members       []string `json:"members"`
+	NumNewMembers string   `json:"num_members"`
+	Description   string   `json:"description"`
+	Etc           string   `json:"etc"`
+}
+
 func SendHelloWorld(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received a request to the root path")
 	w.Write([]byte("Hello, World!"))
 }
 
 func HandleInteraction(w http.ResponseWriter, r *http.Request) {
-	// Parse the form data
 	if err := r.ParseForm(); err != nil {
 		log.Printf("Failed to parse form data: %v", err)
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
 
-	// Extract the payload from the form data
 	payloadStr := r.FormValue("payload")
 	if payloadStr == "" {
 		log.Println("Payload not found in form data")
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
-
-	// Decode the JSON payload
+	log.Println(payloadStr)
 	var payload slack.InteractionCallback
 	if err := json.Unmarshal([]byte(payloadStr), &payload); err != nil {
 		log.Printf("Failed to decode interaction payload: %v", err)
@@ -39,32 +48,52 @@ func HandleInteraction(w http.ResponseWriter, r *http.Request) {
 	handleBlockActions(payload)
 }
 func handleBlockActions(payload slack.InteractionCallback) {
+	log.Println(payload)
+	returnMessage := FormMessage{FormID: "test"}
 	log.Println("Received block actions")
-	for _, actionValues := range payload.View.State.Values {
+	for blockID, actionValues := range payload.View.State.Values {
 		for actionID, blockAction := range actionValues {
 			switch actionID {
 			case "multi_static_select-action":
 				log.Println("Received multi_static_select block action")
+				static_actions := []string{}
 				for _, action := range blockAction.SelectedOptions {
+					static_actions = append(static_actions, action.Value)
 					log.Printf("Received input block action: %s", action.Value)
+				}
+				if blockID == "team_role_block" {
+					returnMessage.TeamRoles = static_actions
+				}
+				if blockID == "tech_stack_block" {
+					returnMessage.TechStacks = static_actions
 				}
 			case "team_intro":
 				log.Println("Received team intro block action")
 				log.Printf("Received input block action: %s", blockAction.Value)
+				returnMessage.TeamIntro = blockAction.Value
 			case "team_name":
 				log.Println("Received team name block action")
 				log.Printf("Received input block action: %s", blockAction.Value)
+				returnMessage.TeamName = blockAction.Value
 			case "multi_users_select-action":
 				log.Println("Received multi_users_select block action")
 				for _, action := range blockAction.SelectedUsers {
+					returnMessage.Members = append(returnMessage.Members, action)
 					log.Printf("Received input block action: %s", action)
 				}
 			case "num_members":
 				log.Println("Received num members block action")
-				log.Printf("Received input block action: %s", blockAction.SelectedOption.Value)
+				log.Printf("Received input block action: %s", blockAction.Value)
+				returnMessage.NumNewMembers = blockAction.Value
 			case "plain_text_input-action":
 				log.Println("Received plain text input block action")
 				log.Printf("Received input block action: %s", blockAction.Value)
+				if blockID == "team_desc_block" {
+					returnMessage.Description = blockAction.Value
+				}
+				if blockID == "team_etc_block" {
+					returnMessage.Etc = blockAction.Value
+				}
 			}
 		}
 	}
@@ -77,14 +106,12 @@ func HandleSlashCommand(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Parse form-encoded data
 	if err := r.ParseForm(); err != nil {
 		log.Printf("Failed to parse form data: %v", err)
 		http.Error(w, "Invalid form data", http.StatusBadRequest)
 		return
 	}
 
-	// Extract relevant fields
 	command := r.FormValue("command")
 	triggerID := r.FormValue("trigger_id")
 
