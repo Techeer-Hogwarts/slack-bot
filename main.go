dc557608242e2304b56aac940b9a728cdc43675f
Ryan <leehahnwoong@gmail.com>
2024-07-29 23:04:21 -0700

    fix: main port

--- main.go
+++ main.go
@@ -3,32 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/thomas-and-friends/slack-bot/cmd"
+	"github.com/thomas-and-friends/slack-bot/db"
 )
 
 var err error
 
 func main() {
-	// port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	http.HandleFunc("/slack/commands", cmd.HandleSlashCommand)
 	http.HandleFunc("/trigger_event", cmd.TriggerEvent)
 	http.HandleFunc("/slack/interactions", cmd.HandleInteraction)
 	http.HandleFunc("/", cmd.SendHelloWorld)
 	http.HandleFunc("/test", cmd.TestEvent)
-	port := "8080"
+	if port == "" {
+		port = "8080"
+	}
 
-	// db.DBMain, err = db.NewSQLDB("pgx")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer func() {
-	// 	if err = db.DBMain.Close(); err != nil {
-	// 		panic(err)
-	// 	}
-	// 	log.Println("Disconnected from SQL Database")
-	// }()
+	db.DBMain, err = db.NewSQLDB("pgx")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = db.DBMain.Close(); err != nil {
+			panic(err)
+		}
+		log.Println("Disconnected from SQL Database")
+	}()
 
 	log.Printf("Server started on port: %v", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
