cfa1ddd4a90a8818a2c1f823c7e91d314f9a173a
Ryan <leehahnwoong@gmail.com>
2024-10-27 01:16:01 -0700

    fix: logs

--- cmd/github.go
+++ cmd/github.go
@@ -29,6 +29,8 @@ func DeployImageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	log.Println(temp.Secret)
+	log.Println(secret)
 	if temp.Secret != secret {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
@@ -51,7 +53,7 @@ func sendDeploymentMessageToChannel(deployMessage deployRequest) error {
 	messageText := "이 메시지는 아래 커밋 메시지에 의해 트리거된 배포 파이프라인 입니다. \n 커밋 링크 & 메시지" + deployMessage.CommitLink + "\n" + deployMessage.Message + "아래 이미지를 배포할까요? \n 이미지 이름: " + deployMessage.ImageName + "\n 이미지 태그: " + deployMessage.ImageTag + "\n"
 	deployButton := slack.NewButtonBlockElement("deploy_button", "apply", slack.NewTextBlockObject("plain_text", ":white_check_mark: 네", false, false))
 	noDeployButton := slack.NewButtonBlockElement("no_deploy_button", "delete", slack.NewTextBlockObject("plain_text", ":no_entry_sign: 아니요", false, false))
-	actionBlock := slack.NewActionBlock("apply_action", deployButton, noDeployButton)
+	actionBlock := slack.NewActionBlock("deploy_action", deployButton, noDeployButton)
 	section := slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", messageText, false, false), nil, nil)
 	messageBlocks := slack.MsgOptionBlocks(section, actionBlock)
 	_, _, err := api.PostMessage(channelID, messageBlocks)
