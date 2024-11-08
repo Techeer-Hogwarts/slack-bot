e2ce82da60567cfe257510bb2794b047b69bc92f
Ryan <leehahnwoong@gmail.com>
2024-11-07 23:14:57 -0800

    feat: add github action sync cd

--- cmd/github.go
+++ cmd/github.go
@@ -53,9 +53,12 @@ func sendDeploymentMessageToChannel(deployMessage deployRequest) error {
 	messageText := "이 메시지는 아래 커밋 메시지에 의해 트리거된 배포 파이프라인 입니다. \n 커밋 링크 & 메시지 \n" + deployMessage.CommitLink + "\n" + "아래 이미지를 배포할까요? \n 이미지 이름: " + deployMessage.ImageName + "\n 이미지 태그: " + deployMessage.ImageTag + "\n"
 	deployButton := slack.NewButtonBlockElement("deploy_button", imageNameWithTag, slack.NewTextBlockObject("plain_text", ":white_check_mark: 네", false, false))
 	noDeployButton := slack.NewButtonBlockElement("no_deploy_button", "delete", slack.NewTextBlockObject("plain_text", ":no_entry_sign: 아니요", false, false))
+	inputElement := slack.NewPlainTextInputBlockElement(slack.NewTextBlockObject("plain_text", "기본값: 1", false, false), "replica_count")
+	inputElement.MaxLength = 2
+	inputBlock := slack.NewInputBlock("replica_action", slack.NewTextBlockObject("plain_text", "복제 컨테이너 개수", false, false), slack.NewTextBlockObject("plain_text", "Scale Out 갯수", false, false), inputElement)
 	actionBlock := slack.NewActionBlock("deploy_action", deployButton, noDeployButton)
 	section := slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", messageText, false, false), nil, nil)
-	messageBlocks := slack.MsgOptionBlocks(section, actionBlock)
+	messageBlocks := slack.MsgOptionBlocks(section, inputBlock, actionBlock)
 	_, _, err := api.PostMessage(channelID, messageBlocks)
 	if err != nil {
 		return err
@@ -67,6 +70,11 @@ func triggerDeployment(actionValue string, payload slack.InteractionCallback) er
 	api := slack.New(botToken)
 	channelID := payload.Channel.ID
 	imageNameWithTag := actionValue
+	pyalodJsonVal, _ := json.MarshalIndent(payload, "", "  ")
+	log.Printf("Payload: %s", pyalodJsonVal)
+	// imageNameAndTag := strings.Split(imageNameWithTag, ":")
+	// imageName := imageNameAndTag[0]
+	// imageTag := imageNameAndTag[1]
 	messageText := "이미지 배포가 요청되었습니다. 이미지 이름: " + imageNameWithTag
 	_, _, err := api.PostMessage(channelID, slack.MsgOptionText(messageText, false))
 	if err != nil {
