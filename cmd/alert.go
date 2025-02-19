eca82860e7418fe34717a9e9937ccc347b25d33b
Ryan <86213086+printSANO@users.noreply.github.com>
2025-02-18 21:05:25 -0800

    Scrum 190  notification (#172)

--- cmd/alert.go
+++ cmd/alert.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -9,20 +11,62 @@ type alertRequest struct {
 	Alert  string
 }
 
+type studySchema struct {
+	ID             int    `json:"id"`
+	Type           string `json:"type"`
+	Name           string `json:"name"`
+	StudyExplain   string `json:"studyExplain"`
+	RecruitNum     int    `json:"recruitNum"`
+	Leader         string `json:"leader"`
+	Email          string `json:"email"`
+	RecruitExplain string `json:"recruitExplain"`
+	NotionLink     string `json:"notionLink"`
+	Goal           string `json:"goal"`
+	Rule           string `json:"rule"`
+}
+
+type projectSchema struct {
+	ID             int      `json:"id"`
+	Type           string   `json:"type"`
+	Name           string   `json:"name"`
+	ProjectExplain string   `json:"projectExplain"`
+	FrontNum       int      `json:"frontNum"`
+	BackNum        int      `json:"backNum"`
+	DataEngNum     int      `json:"dataEngNum"`
+	DevOpsNum      int      `json:"devOpsNum"`
+	uiUxNum        int      `json:"uiUxNum"`
+	Leader         string   `json:"leader"`
+	Email          string   `json:"email"`
+	RecruitExplain string   `json:"recruitExplain"`
+	NotionLink     string   `json:"notionLink"`
+	Stack          []string `json:"stack"`
+}
+
+type userMessageSchema struct {
+	ID             int    `json:"id"`
+	TeamName       string `json:"teamName"`
+	Type           string `json:"type"`
+	LeaderEmail    string `json:"leaderEmail"`
+	ApplicantEmail string `json:"applicantEmail"`
+	Result         string `json:"result"`
+}
+
 func AlertChannelHandler(w http.ResponseWriter, r *http.Request) {
-	// if r.Method != http.MethodPost {
-	// 	http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
-	// 	return
-	// }
-	// log.Println("Alert Channel Handler")
-	// var temp alertRequest
-	// requestBody := r.Body
-	// err := json.NewDecoder(requestBody).Decode(&temp)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-	// 	log.Println(err)
-	// 	return
-	// }
+	if r.Method != http.MethodPost {
+		http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	log.Println("Alert Channel Handler")
+	var temp alertRequest
+	requestBody := r.Body
+	log.Println(requestBody)
+	err := json.NewDecoder(requestBody).Decode(&temp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+
 	// if temp.Secret != secret {
 	// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
 	// 	log.Println("Unauthorized")
@@ -33,19 +77,21 @@ func AlertChannelHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func AlertUserHandler(w http.ResponseWriter, r *http.Request) {
-	// if r.Method != http.MethodPost {
-	// 	http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
-	// 	return
-	// }
-	// log.Println("Alert User Handler")
-	// var temp alertRequest
-	// requestBody := r.Body
-	// err := json.NewDecoder(requestBody).Decode(&temp)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-	// 	log.Println(err)
-	// 	return
-	// }
+	if r.Method != http.MethodPost {
+		http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	log.Println("Alert User Handler")
+	var temp alertRequest
+	requestBody := r.Body
+	log.Println(requestBody)
+	err := json.NewDecoder(requestBody).Decode(&temp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+
 	// if temp.Secret != secret {
 	// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
 	// 	log.Println("Unauthorized")
