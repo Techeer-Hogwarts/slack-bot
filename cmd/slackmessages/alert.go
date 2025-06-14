eaaaff0ab974cf1717c05a0643c4f466ab64e6de
Ryan <leehahnwoong@gmail.com>
2025-06-14 15:59:11 -0700

    feat: complete alert handler

--- cmd/slackmessages/alert.go
+++ cmd/slackmessages/alert.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/Techeer-Hogwarts/slack-bot/cmd/models"
 	"github.com/slack-go/slack"
@@ -49,14 +50,46 @@ func ConstructApplicantAndLeaderMessage(leaderProfile, applicantProfile *slack.U
 	return leaderMessageBlock, applicantMessageBlock, nil
 }
 
-func ConstructProjectMessage(project models.FindMemberSchema) string {
+func ConstructProjectMessage(project models.FindMemberSchema, profileIDs []string) (slack.MsgOption, error) {
 	log.Printf("UserObject: %+v", project)
-	return "Some Message"
+	teamLeaderString := strings.Join(profileIDs, ">, <@")
+	teamLeaderString = "<@" + teamLeaderString + ">"
+	projectMessage := "[" + emoji_people + " *새로운 프로젝트 팀 공고가 올라왔습니다* " + emoji_people + "]\n" +
+		"> " + ":name_badge:" + " *팀 이름* \n " + project.Name + "\n\n\n\n" +
+		"> " + emoji_star + " *팀장* " + teamLeaderString + "\n\n\n\n" +
+		"> " + emoji_notebook + " *팀/프로젝트 설명입니다*\n" + project.ProjectExplain + "\n\n\n\n" +
+		"> " + ":woman-raising-hand:" + " *이런 사람을 원합니다!*\n" + project.RecruitExplain + "\n\n\n\n" +
+		"> " + emoji_stack + " *사용되는 기술입니다*\n" + convertStackToEmojiString(project.Stack) + "\n\n\n" +
+		"> " + emoji_dart + " *모집하는 직군 & 인원*\n" + convertRecruitNumToEmojiString(project) + "\n\n\n\n" +
+		"> " + ":notion:" + " *노션 링크* \n" + project.NotionLink + "\n\n자세한 문의사항은 " + teamLeaderString + " 에게 DM으로 문의 주세요!"
+	section := slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", projectMessage, false, false), nil, nil)
+	applyButton := slack.NewButtonBlockElement("", "apply", slack.NewTextBlockObject("plain_text", ":link: 팀 지원하러 가기!", false, false))
+	applyButton.URL = fmt.Sprintf(redirectURL, project.Type, project.ID)
+	deleteButton := slack.NewButtonBlockElement("delete_button2", project.Email[0], slack.NewTextBlockObject("plain_text", ":warning: 삭제하기!", false, false))
+	actionBlock := slack.NewActionBlock("apply_action", applyButton, deleteButton)
+	messageBlocks := slack.MsgOptionBlocks(section, actionBlock)
+	return messageBlocks, nil
 }
 
-func ConstructStudyMessage(study models.FindMemberSchema) string {
+func ConstructStudyMessage(study models.FindMemberSchema, profileIDs []string) (slack.MsgOption, error) {
 	log.Printf("UserObject: %+v", study)
-	return "Some Message"
+	teamLeaderString := strings.Join(profileIDs, ">, <@")
+	teamLeaderString = "<@" + teamLeaderString + ">"
+	studyMessage := "[" + emoji_people + " *새로운 스터디 팀 공고가 올라왔습니다* " + emoji_people + "]\n" +
+		"> " + ":name_badge:" + " *팀 이름* \n " + study.Name + "\n\n\n\n" +
+		"> " + emoji_star + " *팀장* " + teamLeaderString + "\n\n\n\n" +
+		"> " + emoji_notebook + " *팀/프로젝트 설명입니다*\n" + study.StudyExplain + "\n\n\n\n" +
+		"> " + ":man-raising-hand:" + " *이런 사람을 원합니다!*\n" + study.RecruitExplain + "\n\n\n\n" +
+		"> " + ":pencil:" + " *지켜야 하는 규칙입니다!*\n" + study.Rule + "\n\n\n" +
+		"> " + emoji_dart + " *모집하는 스터디 인원*\n" + strconv.Itoa(study.RecruitNum) + "명\n\n\n\n" +
+		"> " + ":notion:" + " *노션 링크* \n" + study.NotionLink + "\n\n자세한 문의사항은 " + teamLeaderString + " 에게 DM으로 문의 주세요!"
+	section := slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", studyMessage, false, false), nil, nil)
+	applyButton := slack.NewButtonBlockElement("", "apply", slack.NewTextBlockObject("plain_text", ":link: 팀 지원하러 가기!", false, false))
+	applyButton.URL = fmt.Sprintf(redirectURL, study.Type, study.ID)
+	deleteButton := slack.NewButtonBlockElement("delete_button2", study.Email[0], slack.NewTextBlockObject("plain_text", ":warning: 삭제하기!", false, false))
+	actionBlock := slack.NewActionBlock("apply_action", applyButton, deleteButton)
+	messageBlocks := slack.MsgOptionBlocks(section, actionBlock)
+	return messageBlocks, nil
 }
 
 func convertRecruitNumToEmojiString(project models.FindMemberSchema) string {
@@ -64,13 +97,62 @@ func convertRecruitNumToEmojiString(project models.FindMemberSchema) string {
 	if project.FrontNum > 0 {
 		recruitString += ":frontend:" + " " + strconv.Itoa(project.FrontNum) + "명\n"
 	}
+	if project.BackNum > 0 {
+		recruitString += ":backend:" + " " + strconv.Itoa(project.BackNum) + "명\n"
+	}
+	if project.DataEngNum > 0 {
+		recruitString += ":data_engineer:" + " " + strconv.Itoa(project.DataEngNum) + "명\n"
+	}
+	if project.DevOpsNum > 0 {
+		recruitString += ":devops:" + " " + strconv.Itoa(project.DevOpsNum) + "명\n"
+	}
+	if project.FullStack > 0 {
+		recruitString += ":fullstack:" + " " + strconv.Itoa(project.FullStack) + "명\n"
+	}
 	return recruitString
 }
 
 func convertStackToEmojiString(stack []string) string {
+	var backArray []string
+	var frontArray []string
+	var devOpsArray []string
+	var otherArray []string
+	var databaseArray []string
 	var stackString string
+
 	for _, s := range stack {
-		stackString += s + " "
+		category := categoryMap[s]
+		emoji := stackMap[s]
+		switch category {
+		case "BACKEND":
+			backArray = append(backArray, emoji)
+		case "FRONTEND":
+			frontArray = append(frontArray, emoji)
+		case "DEVOPS":
+			devOpsArray = append(devOpsArray, emoji)
+		case "OTHER":
+			otherArray = append(otherArray, emoji)
+		case "DATABASE":
+			databaseArray = append(databaseArray, emoji)
+		default:
+			log.Printf("Unknown category: %s", category)
+			otherArray = append(otherArray, s)
+		}
+	}
+	if len(backArray) > 0 {
+		stackString += ":backend:" + " : " + strings.Join(backArray, " ") + "\n"
+	}
+	if len(frontArray) > 0 {
+		stackString += ":frontend:" + " : " + strings.Join(frontArray, " ") + "\n"
+	}
+	if len(devOpsArray) > 0 {
+		stackString += ":devops:" + " : " + strings.Join(devOpsArray, " ") + "\n"
+	}
+	if len(otherArray) > 0 {
+		stackString += ":other:" + " : " + strings.Join(otherArray, " ") + "\n"
+	}
+	if len(databaseArray) > 0 {
+		stackString += ":database:" + " : " + strings.Join(databaseArray, " ") + "\n"
 	}
 	return stackString
 }
